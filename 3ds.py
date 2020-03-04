4ce3afe9fc92dbd8ca112888ecfb3ed97e0d38e1
CrafterPika <48294025+CrafterPika@users.noreply.github.com>
2020-03-04 09:15:11 +0100

    Hopefully fixed the plugin

--- 3ds.py
+++ 3ds.py
@@ -1,24 +1,23 @@
-from gui.plugins import basePlugin
-from gui.widgets import categoryPage
-from gui.widgets import installedCategoryPage
+from gui.widgets import basePlugin, categoryPage, installedCategoryPage
 from appstore import Appstore
 
-REPO = "https://crafterpika.github.io/3ds-homebrew/"
+3DS_REPO = "http://wiiubru.com/appstore/"
 LIBGET_DIR = "3ds/appstore/.get/packages"
 
 class Plugin(basePlugin.BasePlugin):
 	def __init__(self, app, container):
 		basePlugin.BasePlugin.__init__(self, app, "3ds", container)
-		self.handler = Appstore("3ds", REPO, LIBGET_DIR)
+		self.handler = Appstore("3ds", 3DS_REPO, LIBGET_DIR)
 
 	def get_pages(self):
 		all_frame = categoryPage.CategoryPage(self.app, self.container, self.handler, "3ds - All", self.handler.all)
 		tools_frame = categoryPage.CategoryPage(self.app, self.container, self.handler, "3ds - Tools", self.handler.tools)
 		emus_frame = categoryPage.CategoryPage(self.app, self.container, self.handler, "3ds - Emus", self.handler.emus)
 		games_frame = categoryPage.CategoryPage(self.app, self.container, self.handler, "3ds - Games", self.handler.games)
+		advanced_frame = categoryPage.CategoryPage(self.app, self.container, self.handler, "3ds - Advanced", self.handler.advanced)
 		misc_frame = categoryPage.CategoryPage(self.app, self.container, self.handler, "3ds - Misc.", self.handler.misc)
 		installed_frame = installedCategoryPage.InstalledCategoryPage(self.app, self.container, self.handler, "3ds - Installed", self.handler.all)
-		return [all_frame, tools_frame, emus_frame, games_frame, misc_frame, installed_frame]
+		return [all_frame, tools_frame, emus_frame, games_frame, advanced_frame, misc_frame, installed_frame]
 
 	def exit(self):
 		pass
